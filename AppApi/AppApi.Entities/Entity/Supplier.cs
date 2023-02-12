1cef1a12e751790d2c50f9c8b819759b4a2df62a
vidatquan <quanvdq01@gmail.com>
2023-02-12 10:39:43 +0700

    code

---
+++ AppApi/AppApi.Entities/Entity/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppApi.Entities.Entity
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string SupplierName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ AppApi/AppApi/Controllers/Class1.cs
@@ -0,0 +1,73 @@
+﻿using AppApi.Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AppApi.Controllers
+{
+    public class ShopController : ApiController
+    {
+       // ShopDL cus = new ShopDL();
+
+
+        [HttpPost]
+        [Route("update-shop")]
+        public bool Update(Shop input)
+        {
+            try
+            {
+                return true;
+                //return cus.UpdateDL(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("customer")]
+        public List<Customer> GetShop(Shop input)
+        {
+            try
+            {
+                return cus.GetCustomer(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [Route("delete-shop")]
+        public bool DeleteShop(Customer input)
+        {
+            try
+            {
+                return cus.DeleteDL(input);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        [HttpPost]
+        [Route("add-shop")]
+        public bool RegisterCustomer(Customer input)
+        {
+            try
+            {
+                return cus.RegisterDL(input);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
