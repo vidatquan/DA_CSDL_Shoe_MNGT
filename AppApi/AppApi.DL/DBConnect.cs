d8ba3d45b4a5f76da1c40a18e830f687546c2af6
quanvd <quanvdq01@gmail.com>
2023-05-10 12:46:36 +0700

    db

--- AppApi/AppApi.DL/DBConnect.cs
+++ AppApi/AppApi.DL/DBConnect.cs
@@ -11,6 +11,7 @@ namespace AppApi.DL
     {
         //protected SqlConnection _conn = new SqlConnection("server=171.244.3.173,1433;database=shippers;uid=sa;password=chuongtq@1999;");
         //protected SqlConnection _conn = new SqlConnection(@"Data Source=51THHJ2;Initial Catalog=quanlyshopgiay; Integrated Security=True");
-        protected SqlConnection _conn = new SqlConnection(@"Data Source=ADMIN;Initial Catalog=quanlyshopgiay; Integrated Security=True");
+        //protected SqlConnection _conn = new SqlConnection(@"Data Source=ADMIN;Initial Catalog=quanlyshopgiay; Integrated Security=True");
+        protected SqlConnection _conn = new SqlConnection("server=DESKTOP-0GPARNN\\DEQUI;database=quanlyshopgiay;uid=sa;password=123456;");
     }
 }
